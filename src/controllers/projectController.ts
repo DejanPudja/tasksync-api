21e2cf7992d7a31e6d9350dc47e1725ca009d958
pudja <dejan.pudja@forwardslashny.com>
2025-01-06 15:56:24 +0100

    Added get project by id and delete project functions

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -20,3 +20,68 @@ export const getProjects = async (
     });
   }
 };
+
+export const getProjectById = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const project = await Project.findOne({
+      where: { id },
+    });
+
+    if (!project) {
+      res.status(404).json({
+        message: "Project not exist!",
+        success: false,
+        project: [],
+      });
+      return;
+    }
+
+    res.status(200).json({
+      project: project,
+      success: true,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: "Internal Server Error!",
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const deleteProject = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const project = await Project.findOne({
+      where: { id },
+    });
+
+    if (!project) {
+      res.status(404).json({
+        message: "Project not exist!",
+        success: false,
+      });
+      return;
+    }
+
+    await project.destroy();
+
+    res.status(200).json({
+      message: "Project successfully deleted!",
+      success: true,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: "Internal Server Error!",
+      success: false,
+      error: error.message,
+    });
+  }
+};
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { getProjects } from "../controllers/projectController";
+import {
+  deleteProject,
+  getProjectById,
+  getProjects,
+} from "../controllers/projectController";
 
 const router = Router();
 
 router.get("/", getProjects);
+router.get("/:id", getProjectById);
+router.delete("/delete/:id", deleteProject);
 
 export default router;
